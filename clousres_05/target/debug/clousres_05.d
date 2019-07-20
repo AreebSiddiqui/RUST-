50467f3e4d0981b4100c842b88cb392b3fe17c93
AreebSiddiqui <areeb40@hotmail.com>
2019-07-20 19:32:24 +0500

    v1.0

---
+++ clousres_05/target/debug/clousres_05.d
@@ -0,0 +1,1 @@
+/home/areeb/Rust/clousres_05/target/debug/clousres_05: /home/areeb/Rust/clousres_05/src/main.rs
---
+++ clousres_05/target/debug/deps/clousres_05-47db239a09bf6c1d.d
@@ -0,0 +1,5 @@
+/home/areeb/Rust/clousres_05/target/debug/deps/clousres_05-47db239a09bf6c1d: src/main.rs
+
+/home/areeb/Rust/clousres_05/target/debug/deps/clousres_05-47db239a09bf6c1d.d: src/main.rs
+
+src/main.rs:
