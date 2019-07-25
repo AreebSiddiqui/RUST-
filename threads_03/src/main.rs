22b950f771686a6fa1fd2ed3ba54272317c69300
AreebSiddiqui <areeb40@hotmail.com>
2019-07-26 02:30:58 +0500

    v1

--- threads_03/src/main.rs
+++ threads_03/src/main.rs
@@ -1,3 +1,14 @@
+use std::thread;
+use std::time::Duration;
 fn main (){
-    println!("Github");
+    
+    let v = vec![1,2,3];
+    let handle = thread::spawn(move ||{
+   
+        println!("{:#?}  ",v);
+    
+    });
+    handle.join().unwrap();
 }
+
+
