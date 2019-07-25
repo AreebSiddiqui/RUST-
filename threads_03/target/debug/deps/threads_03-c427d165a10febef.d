08b30749345753cdb2cadcf635d320c2cacd4735
AreebSiddiqui <areeb40@hotmail.com>
2019-07-26 02:08:41 +0500

    v1

---
+++ threads_03/target/debug/deps/threads_03-c427d165a10febef.d
@@ -0,0 +1,5 @@
+/home/areeb/Rust/threads_03/target/debug/deps/threads_03-c427d165a10febef: src/main.rs
+
+/home/areeb/Rust/threads_03/target/debug/deps/threads_03-c427d165a10febef.d: src/main.rs
+
+src/main.rs:
---
+++ threads_03/target/debug/threads_03.d
@@ -0,0 +1,1 @@
+/home/areeb/Rust/threads_03/target/debug/threads_03: /home/areeb/Rust/threads_03/src/main.rs
