47593a04dd9144d8da244bf62874d40f326bda3d
AreebSiddiqui <areeb40@hotmail.com>
2019-07-27 03:35:50 +0500

    v1

---
+++ rust_coding/target/debug/deps/practice-dfae9b7176501ad1.d
@@ -0,0 +1,5 @@
+/home/areeb/Rust/practice/target/debug/deps/practice-dfae9b7176501ad1: src/main.rs
+
+/home/areeb/Rust/practice/target/debug/deps/practice-dfae9b7176501ad1.d: src/main.rs
+
+src/main.rs:
---
+++ rust_coding/target/debug/practice.d
@@ -0,0 +1,1 @@
+/home/areeb/Rust/practice/target/debug/practice: /home/areeb/Rust/practice/src/main.rs
